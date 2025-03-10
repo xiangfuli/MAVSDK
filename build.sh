3f8f6c5c8234cd4cd64a0de2da47b316af63e010
xiangfuli <xiangfuli96@gmail.com>
2025-03-10 01:20:49 -0400

    support building without openssl

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ if [ ! -d $project_dir/build/default/install ]; then
   attempt=0
   max_attempt=5
   while  [ $attempt -lt $max_attempt ]; do
-      cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DBUILD_MAVSDK_SERVER=ON -DCMAKE_INSTALL_PREFIX=$project_dir/build/default/install -DCMAKE_MODULE_PATH="$project_dir/build/default/install/lib/cmake;$project_dir/build/default/third_party/install/lib/cmake;$project_dir/build/default/third_party/install/lib64/cmake" -Bbuild/default -S. && break 
+      cmake -DBUILD_TESTS=OFF -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DBUILD_MAVSDK_SERVER=ON -DCMAKE_INSTALL_PREFIX=$project_dir/build/default/install -DCMAKE_MODULE_PATH="$project_dir/build/default/install/lib/cmake;$project_dir/build/default/third_party/install/lib/cmake;$project_dir/build/default/third_party/install/lib64/cmake" -Bbuild/default -S. && break 
       echo "CMake command failed"
       sleep 2
       attempt=$((attempt+1))
@@ -23,4 +23,3 @@ cmake --build build/default --target install
 # revise the illegal placement inside the $project_dir/build/default/install/lib/cmake/MAVSDK/MAVSDKTargets.cmake
 
 sed -i 's/\$<LINK_ONLY:\([^@]*\)::[^>]*>>/\$<LINK_ONLY:\1>/g'  $project_dir/build/default/install/lib/cmake/MAVSDK/MAVSDKTargets.cmake
-
